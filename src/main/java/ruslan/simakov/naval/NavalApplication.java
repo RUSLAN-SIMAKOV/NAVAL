41a7b1f50dc6235d0485dbaf87975c91287ba90f
Ruslan Simakov <UA667766706@GMAIL.COM>
2026-05-17 18:16:42 +0100

    Initial commit

---
+++ src/main/java/ruslan/simakov/naval/NavalApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.naval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavalApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NavalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruslan/simakov/naval/NavalApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.naval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
